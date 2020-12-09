6c282b82029557435c15c04ad0710b282d8929ab
koseali <kose.ali@std.izu.edu.tr>
2020-12-09 23:29:37 +0300

    add Choose photo on phone library.

--- Instagram_Basic_Clone/UploadViewController.swift
+++ Instagram_Basic_Clone/UploadViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
-
+class UploadViewController: UIViewController ,UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet weak var tfDate: UITextField!
+    
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tfName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gesture)
+    }
+    @objc func chooseImage(){
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
     }
-    */
+
+    @IBAction func clickUpload(_ sender: Any) {
+    }
+    
 
 }
